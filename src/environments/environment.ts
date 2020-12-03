7b14f46df306d6687952ec43945c9210c2da47d2
Ifesinachi Eze <ieze@byteworks.com.ng>
2020-12-03 11:41:06 +0100

    updated config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  baseURL: 'https://demo.intelehealth.org/openmrs/ws/rest/v1',
-  baseURLCoreApp : 'https://demo.intelehealth.org/openmrs/coreapps/diagnoses',
-  baseURLLegacy: 'https://demo.intelehealth.org/openmrs',
+  baseURL: '/v1',
+  baseURLCoreApp : '/coreapps',
+  baseURLLegacy: '/openmrs',
   mindmapURL: 'https://localhost:3004/api'
 };
 
